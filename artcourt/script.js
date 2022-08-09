f4dd9dd7ae8f779cddca65077ea0816b9475d151
Nicolas Robbe <nrobbe@gmail.com>
2022-08-09 11:20:34 -0400

    art court first daytime/nighttime routing logic

---
+++ artcourt/script.js
@@ -0,0 +1,7 @@
+const hours = new Date().getHours()
+const isDayTime = hours > 6 && hours < 20
+
+if (isDayTime) 
+  window.location.href = "https://hoverlay.io/space/ic2"
+else
+  window.location.href = "https://hoverlay.io/space/f46ov"
