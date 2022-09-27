a88c094aac95d322dc22c8f74f135fe80c48821e
Nicolas Robbe <nrobbe@gmail.com>
2022-09-27 10:15:54 -0400

    Update script.js
    iFP Geniuses of Kendal project

--- geniuses-of-kendall/script.js
+++ geniuses-of-kendall/script.js
@@ -1,7 +1,26 @@
-const hours = new Date().getHours()
-const isDayTime = hours > 6 && hours < 20
+// Rotate through 4 experiences, changing each day
+var now = new Date();
+var start = new Date(now.getFullYear(), 0, 0);
+var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+var oneDay = 1000 * 60 * 60 * 24;
+var day = Math.floor(diff / oneDay);
 
-if (isDayTime) 
-  window.location.href = "https://hoverlay.io/space/10853"
-else
-  window.location.href = "https://hoverlay.io/space/10853"
+// Calculate index to change every day, over 4 days
+var experience_index = day % 4
+
+switch (experience_index) {
+  case 0:
+    window.location.href = "https://hoverlay.io/space/10853"
+    break;
+  case 1:
+    window.location.href = "https://hoverlay.io/space/048"
+    break;
+  case 2:
+    window.location.href = "https://hoverlay.io/space/8347"  
+    break;
+  case 3:
+    window.location.href = "https://hoverlay.io/space/40958"    
+    break;
+  default:
+    window.location.href = "https://hoverlay.io/space/10853"
+}
